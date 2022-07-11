498cb737767a5c0ecb4c77f74f18a5a1b79f0e1a
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-12 02:52:30 +0430

    in URLs & Views monthly challenges project challenges app views.py a dictionary of months and their activity was added.

--- URLs & Views/monthly_challenges/challenges/views.py
+++ URLs & Views/monthly_challenges/challenges/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+monthly_challenges={
+    "january" : "Eat no meat!",
+    "february" : "Walk for 20 mins every day!",
+    "march" : "Learn English!",
+    "april" : "Read a book!",
+    "may" : "Learn Django!",
+    "june" : "Consume no sugar!",
+    "july" : "Learn HTML!",
+    "august" : "drink no alcohol!",
+    "september" : "Walk instead of Driving!",
+    "october" : "Tell no lies!",
+    "november" : "Face your fears!",
+    "december" : "Listen to music!"
+}
 
 def monthly_challenge_by_number(request, month):
     return HttpResponse()
