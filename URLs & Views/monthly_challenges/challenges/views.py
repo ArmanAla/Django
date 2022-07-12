287e5638e1204aedee03a267134a4a9cb89faa9d
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-12 09:45:20 +0430

    in URLs & Views monthly challenges project challenges app views.py: reverse() has been imported and used with help of a url's name.

--- URLs & Views/monthly_challenges/challenges/views.py
+++ URLs & Views/monthly_challenges/challenges/views.py
@@ -1,6 +1,6 @@
-from turtle import forward
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 
 monthly_challenges = {
@@ -22,8 +22,9 @@ def monthly_challenge_by_number(request, month_index):
     months = list(monthly_challenges.keys())
     if month_index > len(monthly_challenges):
         return HttpResponseNotFound("Invalid month!")
-    forward_month = months[month_index-1]
-    return HttpResponseRedirect(f"/challenges/{forward_month}")
+    redirect_month = months[month_index-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
